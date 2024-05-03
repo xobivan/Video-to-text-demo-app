4038875cc449fffa006056402897f84564adbd60
xobivan <dvspivak98@gmail.com>
2024-05-03 02:39:02 +0100

    updated main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def load_model():
     return model, tokenizer
 
 def generate_summary(model, tokenizer, text):
-    inputs = tokenizer(str(text), return_tensors="pt", max_length=1024, truncation=True)
-    max_length = 4000 
-    num_beams = 30   
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
+    max_length = 2000  
+    num_beams = 20    
     top_k = 50        
     top_p = 0.95      
     summary_ids = model.generate(
@@ -29,78 +29,48 @@ def generate_summary(model, tokenizer, text):
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
-st.title("Speech Recognition from Video")
-
-uploaded_file = st.file_uploader("Upload a video", type=["mp4"])
-
-def save_video(uploaded_file):
-
-    if uploaded_file is not None:
-        st.video(uploaded_file)
-
-        # Проверяет загружен ли файл
-        if uploaded_file.name:
-
-            # Создаёт директорию temp если она не создана автоматически
-            if not os.path.exists("temp"):
-                os.makedirs("temp")
-
-            # Сохраняет видео во временной дирекстории
-            video_path = os.path.join("temp", uploaded_file.name)
-            with open(video_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-            return video_path
-        else:
-            st.error("Error: No file uploaded")
-
-@st.cache_data     
-def extract_audio(video_path) -> str:
-    video_clip = None
-
-    # Создание обьекта VideoFileClip
+@st.cache_data
+def extract_audio(video_path):
     video_clip = VideoFileClip(video_path)
+    audio_clip = video_clip.audio
+    audio_file_path = "audio.wav"
+    audio_clip.write_audiofile(audio_file_path, codec="pcm_s16le")
+    video_clip.close()
+    rm_temp_files(video_path)
+    return audio_file_path
 
-    if video_clip is not None:
-
-        # Извлечение аудио из видео
-        audio_clip = video_clip.audio
-        audio_file_path = "audio.wav"
-        audio_clip.write_audiofile(audio_file_path, codec="pcm_s16le")
-        video_clip.close()
-        rm_temp_files(video_path)
-
-        return audio_file_path
+def rm_temp_files(file):
+    os.remove(file)
 
 @st.cache_resource
-def transcribe_audio(audio_file_path) -> str:
-
-    # Запуск модели
+def transcribe_audio(audio_file_path):
     model = whisper.load_model("base")
     result = model.transcribe(audio_file_path, fp16=False)
     rm_temp_files(audio_file_path)
-    return result
-
-def rm_temp_files(file):
-        
-    # Удаление файла
-    os.remove(file)
-
+    return result['text']
 
 def main():
+    st.title("Speech Recognition from Video")
+    uploaded_file = st.file_uploader("Upload a video", type=["mp4"])
+    
     if uploaded_file:
-        video_path = save_video(uploaded_file)
-        if video_path:
-            st.write("Extracting audio...")
-            audio_file_path = extract_audio(video_path)
-            if audio_file_path:
-                st.write("Transcribing...")
-                text = transcribe_audio(audio_file_path)
-                if text:
-                    model, tokenizer = load_model()
-                    st.write("Summarising...")
-                    summary = generate_summary(model, tokenizer, text)
-                    st.success("Your video was transcribed and summarised")
-                    st.write(summary)
+        video_path = os.path.join("temp", uploaded_file.name)
+        with open(video_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        st.video(uploaded_file)
+        st.write("Extracting audio...")
+        audio_file_path = extract_audio(video_path)
+        st.empty()
+
+        st.write("Transcribing...")
+        text = transcribe_audio(audio_file_path)
+        # st.write(text)
+        st.empty()
+
+        model, tokenizer = load_model()
+        summary = generate_summary(model, tokenizer, text)
+        st.write("Summary:", summary)
 
 if __name__ == "__main__":
     main()
